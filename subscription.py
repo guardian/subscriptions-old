1ce6d02030d7f98dba67005265b891f57bb67cb8
Francis Rhys-Jones <francis.rhys-jones@guardian.co.uk>
2012-07-23 14:26:51 +0100

    moved offer page to offer/TAA01

--- subscription.py
+++ subscription.py
@@ -5,20 +5,13 @@ import os
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
-class Index(webapp2.RequestHandler):
+class Page(webapp2.RequestHandler):
     def get(self, r):
         r = r or 'index'
         method = self.request.get('method', 'voucher')
         template = jinja_environment.get_template(r + '.html')
         self.response.out.write(template.render(method = method))
 
-class Promo(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('promo.html')
-        self.response.out.write(template.render())
-
-
 app = webapp2.WSGIApplication([
-	('/promo', Promo),
-    ('/(.*)', Index)
+    ('/(.*)', Page)
 ], debug=True)
