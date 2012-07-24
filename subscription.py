5f593c23c983849f0982d1f1ee46cd23b1eaaec0
Francis Rhys-Jones <francis.rhys-jones@guardian.co.uk>
2012-07-24 13:32:03 +0100

    added omniture tracking

--- subscription.py
+++ subscription.py
@@ -6,12 +6,13 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
 class Page(webapp2.RequestHandler):
-    def get(self, r):
-        r = r or 'index'
+    def get(self, page, promoCode = None):
+        page = page or 'index'
         method = self.request.get('method', 'voucher')
-        template = jinja_environment.get_template(r + '.html')
-        self.response.out.write(template.render(method = method))
+        template = jinja_environment.get_template(page + '.html')
+        self.response.out.write(template.render(method = method, promoCode = promoCode, page = page))
 
 app = webapp2.WSGIApplication([
-    ('/(.*)', Page)
+    ('/(.*?/(.*?))', Page),
+    ('/(.*?)', Page)
 ], debug=True)
