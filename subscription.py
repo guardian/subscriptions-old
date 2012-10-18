de3f21c51eb26dfe627af9ce3267b99da27287ca
Francis Rhys-Jones <francis.rhys-jones@guardian.co.uk>
2012-10-18 18:20:37 +0100

    added additional promo codes FBA01, TCA01

--- subscription.py
+++ subscription.py
@@ -9,16 +9,24 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
 class Page(webapp2.RequestHandler):
-    def get(self, page, promoCode = None):
-        page = page or 'index'
+    def __init__(self, request, response):
+        self.initialize(request, response)
+        self.templateDict = {
+            None : "offer/default-subscription.html",
+            "FBA01" : "offer/default-subscription.html",
+            "TAA01" : "offer/default-offer.html",
+            "TCA01" : "offer/default-offer.html"
+        }
+
+    def get(self, promoCode = None):
         method = self.request.get('method', 'voucher')
-        try:
-            template = jinja_environment.get_template(page + '.html')
-            self.response.out.write(template.render(method = method, promoCode = promoCode, page = page))
-        except TemplateNotFound:
+        if(promoCode in self.templateDict) :
+            template = jinja_environment.get_template(self.templateDict[promoCode])
+            self.response.out.write(template.render(method = method, promoCode = promoCode))
+        else:
             self.redirect('/')
 
 app = webapp2.WSGIApplication([
-    ('/(.*?/(.*?))', Page),
-    ('/(.*?)', Page)
+    ('/.*?/(.*?)', Page),
+    ('/.*?', Page)
 ], debug=True)
