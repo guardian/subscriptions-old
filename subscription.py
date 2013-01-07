0f6cee89e8557d440072b736923902c35ccd1726
James Gorrie <james.gorrie@guardian.co.uk>
2013-01-07 17:22:40 +0000

    Changed URL structure slightly and added new offer

--- subscription.py
+++ subscription.py
@@ -11,22 +11,57 @@ jinja_environment = jinja2.Environment(
 class Page(webapp2.RequestHandler):
     def __init__(self, request, response):
         self.initialize(request, response)
-        self.templateDict = {
-            None : "offer/default-subscription.html",
-            "FBA01" : "offer/default-subscription.html",
-            "TAA01" : "offer/default-offer.html",
-            "TCA01" : "offer/default-offer.html"
+        self.offers = {
+            None : 'offer/default-subscription.html',
+            'offer/FBA01' : {
+                'template': 'offer/default-subscription.html',
+                'promoCode': 'FBA01',
+            },
+            'offer/TAA01' : {
+                'template': 'offer/default-voucher.html',
+                'promoCode': 'TAA01',
+            },
+            'offer/TCA01' : {
+                'template': 'offer/2013.html',
+                'promoCode': 'TCA01',
+            },
+            'paper2013': {
+                'template': 'offer/2013.html',
+                'promoCode': 'PBA01',
+            },
+            'press2012': {
+                'template': 'offer/2013.html',
+                'promoCode': 'PBA02',
+            },
+            'email2013': {
+                'template': 'offer/2013.html',
+                'promoCode': 'PDA01',
+            },
+            'letter2013': {
+                'template': 'offer/2013.html',
+                'promoCode': 'PCA01',
+            },
+            'web2013': {
+                'template': 'offer/2013.html',
+                'promoCode': 'PGA01',
+            },
+            'ppc2013': {
+                'template': 'offer/2013.html',
+                'promoCode': 'PHA01',
+            },
         }
 
-    def get(self, promoCode = None):
+    def get(self, offer_key = None):
         method = self.request.get('method', 'voucher')
-        if(promoCode in self.templateDict) :
-            template = jinja_environment.get_template(self.templateDict[promoCode])
-            self.response.out.write(template.render(method = method, promoCode = promoCode))
-        else:
+
+        if(offer_key in self.offers) :
+            offer = self.offers.get(offer_key)
+            template = jinja_environment.get_template(offer.get('template'))
+            self.response.out.write(template.render(method = method, promoCode = offer.get('promoCode')))
+        elif promoCode:
             self.redirect('/')
 
 app = webapp2.WSGIApplication([
-    ('/.*?/(.*?)', Page),
-    ('/.*?', Page)
+    ('/', Page),
+    ('/(.*?)', Page),
 ], debug=True)
