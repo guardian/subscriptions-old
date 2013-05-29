1a86a62b3074fec8b1d061069f915b81f7ff211c
Adam Fisher <adam.fisher@guardian.co.uk>
2013-05-29 14:56:19 +0100

    Refactor for new landing page
    The new subscriptions app is a simple landing page. As such, this
    commit removes the old templates and greatly simplifies the app.
    
    A new landing page template has been created but the designs are not
    final so the cut-up hasn't started beyond broadly adding the main
    sections so there's a real page there to check.

--- subscription.py
+++ subscription.py
@@ -8,63 +8,18 @@ import webapp2
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
-class Page(webapp2.RequestHandler):
+class LandingPage(webapp2.RequestHandler):
     def __init__(self, request, response):
         self.initialize(request, response)
-        self.offers = {
-            None : {
-                'template': 'offer/default.html',
-                'promoCode': 'FAA02',
-            },
-            'offer/FBA01' : {
-                'template': 'offer/default-subscription.html',
-                'promoCode': 'FBA01',
-            },
-            'offer/TAA01' : {
-                'template': 'offer/default-voucher.html',
-                'promoCode': 'TAA01',
-            },
-            'offer/TCA01' : {
-                'template': 'offer/default-voucher.html',
-                'promoCode': 'TCA01',
-            },
-            'paper2013': {
-                'template': 'offer/2013.html',
-                'promoCode': 'PBA01',
-            },
-            'press2013': {
-                'template': 'offer/2013.html',
-                'promoCode': 'PBA02',
-            },
-            'email2013': {
-                'template': 'offer/2013.html',
-                'promoCode': 'PDA01',
-            },
-            'letter2013': {
-                'template': 'offer/2013.html',
-                'promoCode': 'PCA01',
-            },
-            'web2013': {
-                'template': 'offer/2013.html',
-                'promoCode': 'PGA01',
-            },
-            'ppc2013': {
-                'template': 'offer/2013.html',
-                'promoCode': 'PHA01',
-            },
-        }
 
-    def get(self, offer_key = None):
-        method = self.request.get('method', 'voucher')
-
-        if(offer_key in self.offers) :
-            offer = self.offers.get(offer_key)
-            template = jinja_environment.get_template(offer.get('template'))
-            self.response.out.write(template.render(method = method, promoCode = offer.get('promoCode')))
-        elif offer_key:
-            self.redirect('/')
+    def get(self, legacy_offer_key = None):
+        if legacy_offer_key:
+            self.redirect('/', permanent=True)
+        else:
+            template = jinja_environment.get_template('landing_page.html')
+            self.response.out.write(template.render())
 
 app = webapp2.WSGIApplication([
-    ('/', Page),
-    ('/(.*?)', Page),
+    ('/', LandingPage),
+    ('/(.*?)', LandingPage),
 ], debug=True)
