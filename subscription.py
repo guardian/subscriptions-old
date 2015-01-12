62509499725b461bf93aad4ebe3f7dbf308d4e9e
Marc Hibbins <marc.hibbins@guardian.co.uk>
2015-01-12 12:07:24 +0000

    Add initial bundle selection pages

--- subscription.py
+++ subscription.py
@@ -24,6 +24,51 @@ editions = {
     },
 }
 
+bundles = {
+    'collection-paper-digital': {
+        'method': 'collection',
+        'package': 'paper-digital',
+        'name': 'Paper + digital subscription',
+        'packages': {
+            'everyday': 'https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=gv7&skip=1',
+            'sixday': 'https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=gv6&skip=1',
+            'weekend': 'https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=gv2&skip=1',
+            'sunday': 'https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx01&title=ov1&skip=1',
+        }
+    },
+    'collection-paper': {
+        'method': 'collection',
+        'package': 'paper',
+        'name': 'Paper subscription',
+        'packages': {
+            'everyday': 'https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx00&title=gv7&skip=1',
+            'sixday': 'https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx00&title=gv6&skip=1',
+            'weekend': 'https://www.guardiansubscriptions.co.uk/Voucher?prom=faa03&pkgcode=ukx00&title=gv2&skip=1',
+        }
+    },
+    'delivery-paper-digital': {
+        'method': 'delivery',
+        'package': 'paper-digital',
+        'name': 'Paper + digital subscription',
+        'packages': {
+            'everyday': 'https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=EVERYDAY%2B',
+            'sixday': 'https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=SIXDAY%2B',
+            'weekend': 'https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=WEEKEND%2B',
+            'sunday': 'https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=SUNDAY%2B',
+        }
+    },
+    'delivery-paper': {
+        'method': 'delivery',
+        'package': 'paper',
+        'name': 'Paper subscription',
+        'packages': {
+            'everyday': 'https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=EVERYDAY',
+            'sixday': 'https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=SIXDAY',
+            'weekend': 'https://www.guardiandirectsubs.co.uk/Delivery/details.aspx?package=WEEKEND',
+        }
+    },
+}
+
 
 class SimpleTemplate(webapp2.RequestHandler):
     template_path = ''
@@ -74,12 +119,47 @@ class DigitalPackAU(SimpleTemplate):
     template_path = 'digital-pack.html'
 
 
+class BundleForm(SimpleTemplate):
+    def post(self):
+        bundle = self.request.get('bundle')
+        method, package = bundle.rsplit('-', 1)
+        try:
+            url = bundles[method]['packages'][package]
+            return webapp2.redirect(url)
+        except KeyError:
+            return webapp2.redirect(self.request.referer)
+
+
+class CollectionDigitalPaper(BundleForm):
+    context = {'bundle': bundles['collection-paper-digital']}
+    template_path = 'choose-bundle.html'
+
+
+class CollectionPaper(BundleForm):
+    context = {'bundle': bundles['collection-paper']}
+    template_path = 'choose-bundle.html'
+
+
+class DeliveryDigitalPaper(BundleForm):
+    context = {'bundle': bundles['delivery-paper-digital']}
+    template_path = 'choose-bundle.html'
+
+
+class DeliveryPaper(BundleForm):
+    context = {'bundle': bundles['delivery-paper']}
+    template_path = 'choose-bundle.html'
+
+
 app = webapp2.WSGIApplication([
     ('/us/digitalpack', DigitalPackUS),
     ('/us', SubscriptionsUS),
     ('/au/digitalpack', DigitalPackAU),
     ('/au', SubscriptionsAU),
     ('/digitalpack', DigitalPackUK),
+    ('/collection/paper-digital', CollectionDigitalPaper),
+    ('/collection/paper', CollectionPaper),
+    ('/delivery/paper-digital', DeliveryDigitalPaper),
+    ('/delivery/paper', DeliveryPaper),
     ('/', SubscriptionsUK),
     ('/(.*?)', SubscriptionsUK),
 ], debug=True)
